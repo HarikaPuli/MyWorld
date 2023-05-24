070cdc160dd22c63c116ce8393ecaa86f3fa634f
Harika <harikapuli95@gmail.com>
2023-05-24 23:42:10 +0530

    Added:Management app

---
+++ React/management/src/GenericUser.js
@@ -0,0 +1,13 @@
+function GenericUser(props) {
+    console.log("props", props);
+    return (
+        <div>
+            <h1>Generic User</h1>
+            {props.data.name}<br></br>
+            {props.data.address}<br></br>
+            {props.data.pincode}<br></br>
+
+        </div>
+    )
+}
+export default GenericUser;
